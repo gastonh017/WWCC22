be1632806d35d0cd9ce93f3213d33b527676ee55
DamianMuracciole <damianmuracciole@gmail.com>
2022-09-19 18:56:41 -0300

    acomodando código en Games

---
+++ src/Components/CardsMatch.js
@@ -0,0 +1,53 @@
+import React, {useState} from "react";
+import {Card, ListGroup, Button , Offcanvas} from 'react-bootstrap';
+
+export default function Cards (props) {
+    
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
+    return(
+      // 'primary', => Azul
+      // 'secondary', => gris oscuro
+      //   'Success', => verde
+      //   'Danger', => rojo
+      //   'Warning', => amarillo
+      //   'Info', => celeste
+      //   'Light', => gris claro
+      //   'Dark', => negro
+  
+      <Card className="my-bg-card  text-dark margin_card text-center" style={{ width: '14rem'}}>
+        <Card.Header className=" my-bg-card text-light " >Match {props.index + props.offset}</Card.Header>
+        <ListGroup variant="flush">
+          <ListGroup.Item className="my-bg-card text-light strong" >
+            {props.home_team_code} <strong style={{ fontSize: '2rem'}}>{props.home_team_goals}</strong> - <strong style={{ fontSize: '2rem'}}>{props.away_team_goals}</strong> {props.away_team_code}
+          </ListGroup.Item>
+          
+          <ListGroup.Item className="my-bg-card text-light weak" >{props.datetime.slice(0,10)}</ListGroup.Item>
+          <ListGroup.Item className="my-bg-card text-light weak" >{props.datetime.slice(11,16)}</ListGroup.Item>
+          
+          <Button variant="primary" onClick={handleShow}>
+            More information ...
+          </Button>
+  
+          <Offcanvas show={show} onHide={handleClose} placement={'top'} className="my-bg-offcanvass" >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title>
+                MATCH {props.index + props.offset}: {props.home_team_country} Vs. {props.away_team_country}
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <h6>Result: {props.home_team_code} {props.home_team_goals} - {props.away_team_goals} {props.away_team_code} </h6>
+              <p className='parrafo-canvas'> Date : {props.datetime.slice(0,10)} - Time: {props.datetime.slice(11,16)} Hs. </p>
+              <p className='parrafo-canvas'> Stadium :{props.stadium}</p>
+              <p className='parrafo-canvas'> City : {props.city}</p>
+            </Offcanvas.Body>
+          </Offcanvas>
+  
+  
+        </ListGroup>
+  
+      </Card>
+    )
+  }
--- src/Components/Games.js
+++ src/Components/Games.js
@@ -1,241 +1,175 @@
 import React, {useState, useEffect} from "react";
-
-// React-Bootstrap
-import {Card, ListGroup, Button , Offcanvas} from 'react-bootstrap';
-
-const Cards = (props) => {
-    
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  return(
-    // 'primary', => Azul
-    // 'secondary', => gris oscuro
-    //   'Success', => verde
-    //   'Danger', => rojo
-    //   'Warning', => amarillo
-    //   'Info', => celeste
-    //   'Light', => gris claro
-    //   'Dark', => negro
-
-    <Card className="my-bg-card  text-dark margin_card text-center" style={{ width: '14rem'}}>
-      <Card.Header className=" my-bg-card text-light " >Match{props.index + props.offset}</Card.Header>
-      <ListGroup variant="flush">
-        <ListGroup.Item className="my-bg-card text-light strong" >
-          {props.home_team_code} <strong style={{ fontSize: '2rem'}}>{props.home_team_goals}</strong> - <strong style={{ fontSize: '2rem'}}>{props.away_team_goals}</strong> {props.away_team_code}
-        </ListGroup.Item>
-        
-        <ListGroup.Item className="my-bg-card text-light weak" >{props.datetime.slice(0,10)}</ListGroup.Item>
-        <ListGroup.Item className="my-bg-card text-light weak" >{props.datetime.slice(11,16)}</ListGroup.Item>
-        
-        <Button variant="primary" onClick={handleShow}>
-          Más info ...
-        </Button>
-
-        <Offcanvas show={show} onHide={handleClose} placement={'top'} className="my-bg-offcanvass" >
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title>
-              MATCH {props.index + props.offset}: {props.home_team_country} Vs. {props.away_team_country}
-            </Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <h6>Result: {props.home_team_code} {props.home_team_goals} - {props.away_team_goals} {props.away_team_code} </h6>
-            <p className='parrafo-canvas'> Date : {props.datetime.slice(0,10)} - Time: {props.datetime.slice(11,16)} Hs. </p>
-            <p className='parrafo-canvas'> Stadium :{props.stadium}</p>
-            <p className='parrafo-canvas'> City : {props.city}</p>
-          </Offcanvas.Body>
-        </Offcanvas>
+import Loading from './Loading';
+import Cards from './CardsMatch'
 
 
-      </ListGroup>
-
-    </Card>
-  )
-}
 
 export default function Games() {
-    const [fase, setFase]       = useState([])
-    const [octavos, setOctavos] = useState([])
-    const [cuartos, setCuartos] = useState([])
-    const [semis, setSemis]     = useState([])
-    const [tercero, setTercero] = useState([])
-    const [final,setFinal]      = useState([])
+  const [fase, setFase]       = useState([])
+  const [octavos, setOctavos] = useState([])
+  const [cuartos, setCuartos] = useState([])
+  const [semis, setSemis]     = useState([])
+  const [tercero, setTercero] = useState([])
+  const [final,setFinal]      = useState([])
 
+  useEffect(() => {
+    fetch('https://worldcup.sfg.io/matches')
+    .then(response => response.json())
+    .then(data => {
+      setFase(data.filter(match => match.stage_name === "First Stage"));
+      setOctavos(data.filter(match => match.stage_name === "Round of 16"));
+      setCuartos(data.filter(match => match.stage_name === "Quarter-final"));
+      setSemis(data.filter(match => match.stage_name === "Semi-final"));
+      setTercero(data.filter(match => match.stage_name === "Match for third place"));
+      setFinal(data.filter(match => match.stage_name === "Final"));
+    })
+    .catch(error => console.log(error));
+  },[])
 
-    useEffect(() => {
-        fetch('https://worldcup.sfg.io/matches')
-        .then(response => response.json())
-        .then(data => {
-            setFase(data.filter(match => match.stage_name === "First Stage"));
-            setOctavos(data.filter(match => match.stage_name === "Round of 16"));
-            setCuartos(data.filter(match => match.stage_name === "Quarter-final"));
-            setSemis(data.filter(match => match.stage_name === "Semi-final"));
-            setTercero(data.filter(match => match.stage_name === "Match for third place"));
-            setFinal(data.filter(match => match.stage_name === "Final"));
-        })
-        .catch(error => console.log(error));
-    },[])
-
-    return (
+  return (
     <div className = "container back-img">
 
-        <h1 className='my-main-title' >WorldCup 2019</h1>
+      <h1 className='my-main-title' >WorldCup 2019</h1>
 
-        <div className = "row justify-content-evenly">
+      <div className = "row justify-content-evenly">
 
-            <h3 className="mt-4 my-secondary-title">Fase de grupos</h3>
-            {
-              fase.length === 0 ?
-                
-              <div class="spinner-border" role="status">
-                <span class="visually-hidden">Loading...</span>
-              </div>
-                
+        <h3 className="mt-4 my-secondary-title">Fase de grupos</h3>
+        
+        { fase.length === 0 ?
+            
+          <Loading />
+    
+          : fase.map((element,index)=>
+            <Cards key = {index}
+              index = {index}
+              offset = {1}
+              home_team_code = {element.home_team.code}
+              away_team_code = {element.away_team.code}
+              datetime = {element.datetime}
+              home_team_country = {element.home_team_country}
+              away_team_country = {element.away_team_country}
+              home_team_goals = {element.home_team.goals}
+              away_team_goals = {element.away_team.goals}
+              city = {element.venue}
+              stadium = {element.location}
+            />
+          )
+        }
 
+        <h3 className="mt-4 my-secondary-title">Octavos de final</h3>
+        { fase.length === 0 ?
+
+          <Loading />
+          
+          :octavos.map((element,index)=>
+            <Cards key = {index + 37}
+              index = {index}
+              offset = {37}
+              home_team_code = {element.home_team.code}
+              away_team_code = {element.away_team.code}
+              datetime = {element.datetime}
+              home_team_country = {element.home_team_country}
+              away_team_country = {element.away_team_country}
+              home_team_goals = {element.home_team.goals}
+              away_team_goals = {element.away_team.goals}
+              city = {element.venue}
+              stadium = {element.location}
+            />
+          )
+        }
+
+        <h3 className="mt-4 my-secondary-title">Cuartos de final</h3>
+        
+        { fase.length === 0 ?
+            
+          <Loading />
               
-                : fase.map((element,index)=>
-                  <Cards key = {index}
-                    index = {index}
-                    offset = {1}
-                    home_team_code = {element.home_team.code}
-                    away_team_code = {element.away_team.code}
-                    datetime = {element.datetime}
-                    home_team_country = {element.home_team_country}
-                    away_team_country = {element.away_team_country}
-                    home_team_goals = {element.home_team.goals}
-                    away_team_goals = {element.away_team.goals}
-                    city = {element.venue}
-                    stadium = {element.location}
-                />)
-            }
+          : cuartos.map((element,index)=>
+            <Cards key = {index + 45}
+              index = {index}
+              offset = {45}
+              home_team_code = {element.home_team.code}
+              away_team_code = {element.away_team.code}
+              datetime = {element.datetime}
+              home_team_country = {element.home_team_country}
+              away_team_country = {element.away_team_country}
+              home_team_goals = {element.home_team.goals}
+              away_team_goals = {element.away_team.goals}
+              city = {element.venue}
+              stadium = {element.location}
+            />
+          )
+        }
 
-            <h3 className="mt-4 my-secondary-title">Octavos de final</h3>
-            {fase.length === 0 ?
-                
-                <div class="spinner-border" role="status">
-                  <span class="visually-hidden">Loading...</span>
-                </div>
-                  
-  
-                
-                  :octavos.map((element,index)=>
-              <Cards key = {index + 37}
-                index = {index}
-                offset = {37}
-                home_team_code = {element.home_team.code}
-                away_team_code = {element.away_team.code}
-                datetime = {element.datetime}
-                home_team_country = {element.home_team_country}
-                away_team_country = {element.away_team_country}
-                home_team_goals = {element.home_team.goals}
-                away_team_goals = {element.away_team.goals}
-                city = {element.venue}
-                stadium = {element.location}
-              />
-            )}
+        <h3 className="mt-4 my-secondary-title">Semi-finales</h3>
+        
+        { fase.length === 0 ?
+            
+          <Loading />
+          
+          : semis.map((element,index)=>
+            <Cards key = {index + 49}
+              index = {index}
+              offset = {49}
+              home_team_code = {element.home_team.code}
+              away_team_code = {element.away_team.code}
+              datetime = {element.datetime}
+              home_team_country = {element.home_team_country}
+              away_team_country = {element.away_team_country}
+              home_team_goals = {element.home_team.goals}
+              away_team_goals = {element.away_team.goals}
+              city = {element.venue}
+              stadium = {element.location}
+            />
+          )
+        }
 
-            <h3 className="mt-4 my-secondary-title">Cuartos de final</h3>
-            {fase.length === 0 ?
-                
-                <div class="spinner-border" role="status">
-                  <span class="visually-hidden">Loading...</span>
-                </div>
-                  
-  
-                
-                  :cuartos.map((element,index)=>
-              <Cards key = {index + 45}
-                index = {index}
-                offset = {45}
-                home_team_code = {element.home_team.code}
-                away_team_code = {element.away_team.code}
-                datetime = {element.datetime}
-                home_team_country = {element.home_team_country}
-                away_team_country = {element.away_team_country}
-                home_team_goals = {element.home_team.goals}
-                away_team_goals = {element.away_team.goals}
-                city = {element.venue}
-                stadium = {element.location}
-              />
-            )}
+        <h3 className="mt-4 my-secondary-title">Tercer puesto</h3>
+        { fase.length === 0 ?
+            
+          <Loading />
+              
+          : tercero.map((element,index)=>
+            <Cards key = {index + 51}
+              index = {index}
+              offset = {51}
+              home_team_code = {element.home_team.code}
+              away_team_code = {element.away_team.code}
+              datetime = {element.datetime}
+              home_team_country = {element.home_team_country}
+              away_team_country = {element.away_team_country}
+              home_team_goals = {element.home_team.goals}
+              away_team_goals = {element.away_team.goals}
+              city = {element.venue}
+              stadium = {element.location}
+            />
+          )
+        }
 
-            <h3 className="mt-4 my-secondary-title">Semi-finales</h3>
-            {fase.length === 0 ?
-                
-                <div class="spinner-border" role="status">
-                  <span class="visually-hidden">Loading...</span>
-                </div>
-                  
-  
-                
-                  :semis.map((element,index)=>
-              <Cards key = {index + 49}
-                index = {index}
-                offset = {49}
-                home_team_code = {element.home_team.code}
-                away_team_code = {element.away_team.code}
-                datetime = {element.datetime}
-                home_team_country = {element.home_team_country}
-                away_team_country = {element.away_team_country}
-                home_team_goals = {element.home_team.goals}
-                away_team_goals = {element.away_team.goals}
-                city = {element.venue}
-                stadium = {element.location}
-              />
-            )}
+        <h3 className="mt-4 my-secondary-title">Final</h3>
+        
+        { fase.length === 0 ?
+            
+          <Loading />
+              
+          : final.map((element,index)=>
+            <Cards key = {index + 52}
+              index = {index}
+              offset = {52}
+              home_team_code = {element.home_team.code}
+              away_team_code = {element.away_team.code}
+              datetime = {element.datetime}
+              home_team_country = {element.home_team_country}
+              away_team_country = {element.away_team_country}
+              home_team_goals = {element.home_team.goals}
+              away_team_goals = {element.away_team.goals}
+              city = {element.venue}
+              stadium = {element.location}
+            />
+          )
+        }
 
-            <h3 className="mt-4 my-secondary-title">Tercer puesto</h3>
-            {fase.length === 0 ?
-                
-                <div class="spinner-border" role="status">
-                  <span class="visually-hidden">Loading...</span>
-                </div>
-                  
-  
-                
-                  :tercero.map((element,index)=>
-              <Cards key = {index + 51}
-                index = {index}
-                offset = {51}
-                home_team_code = {element.home_team.code}
-                away_team_code = {element.away_team.code}
-                datetime = {element.datetime}
-                home_team_country = {element.home_team_country}
-                away_team_country = {element.away_team_country}
-                home_team_goals = {element.home_team.goals}
-                away_team_goals = {element.away_team.goals}
-                city = {element.venue}
-                stadium = {element.location}
-              />
-            )}
-
-            <h3 className="mt-4 my-secondary-title">Final</h3>
-            {fase.length === 0 ?
-                
-                <div class="spinner-border" role="status">
-                  <span class="visually-hidden">Loading...</span>
-                </div>
-                  
-  
-                
-                  :final.map((element,index)=>
-              <Cards key = {index + 52}
-                index = {index}
-                offset = {52}
-                home_team_code = {element.home_team.code}
-                away_team_code = {element.away_team.code}
-                datetime = {element.datetime}
-                home_team_country = {element.home_team_country}
-                away_team_country = {element.away_team_country}
-                home_team_goals = {element.home_team.goals}
-                away_team_goals = {element.away_team.goals}
-                city = {element.venue}
-                stadium = {element.location}
-              />
-            )}
-
-        </div>
+      </div>
     </div>
-    )
+  )
 }
